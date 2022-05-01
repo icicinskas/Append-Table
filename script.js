898ebb6b9ffb150f318703bf8a644a7add62d742
Igoris Cicinskas <igoris.cic@gmail.com>
2022-05-01 21:35:30 +0300

    format style

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
-"use strict"
+"use strict";
 
-var prideti = document.getElementById("prideti")
-var istrintiPirma = document.getElementById("istrintiPirma")
-var istrintiPaskutini = document.getElementById("istrintiPaskutini")
-var forma = document.querySelector("form")
-var tbodyEl = document.querySelector("tbody")
-var vardas = document.getElementById("names")
-var pavarde = document.getElementById("lastnames")
-var amzius = document.getElementById("age")
+var prideti = document.getElementById("prideti");
+var istrintiPirma = document.getElementById("istrintiPirma");
+var istrintiPaskutini = document.getElementById("istrintiPaskutini");
+var forma = document.querySelector("form");
+var tbodyEl = document.querySelector("tbody");
+var vardas = document.getElementById("names");
+var pavarde = document.getElementById("lastnames");
+var amzius = document.getElementById("age");
 
 prideti.addEventListener("click", function (e) {
-    e.preventDefault()
+  e.preventDefault();
 
-    if (vardas.value && pavarde.value && amzius.value && amzius.value > 0) {
+  if (vardas.value && pavarde.value && amzius.value && amzius.value > 0) {
+    var newRow = document.createElement("tr");
+    tbodyEl.append(newRow);
+    var cell1 = document.createElement("td");
+    var cell2 = document.createElement("td");
+    var cell3 = document.createElement("td");
 
-        var newRow = document.createElement("tr")
-        tbodyEl.append(newRow)
-        var cell1 = document.createElement("td")
-        var cell2 = document.createElement("td")
-        var cell3 = document.createElement("td")
+    cell1.innerText = vardas.value;
+    cell2.innerText = pavarde.value;
+    cell3.innerText = amzius.value;
+    newRow.append(cell1, cell2, cell3);
+  } else {
+    alert("Įveskite duomenis");
+    return;
+  }
 
-        cell1.innerText = vardas.value
-        cell2.innerText = pavarde.value
-        cell3.innerText = amzius.value
-        newRow.append(cell1, cell2, cell3)
-
-    } else {
-        alert("Įveskite duomenis")
-        return
-    }
-
-    vardas.value = ""
-    pavarde.value = ""
-    amzius.value = ""
-
-})
+  vardas.value = "";
+  pavarde.value = "";
+  amzius.value = "";
+});
 
 istrintiPirma.addEventListener("click", function (e) {
-    e.preventDefault()
+  e.preventDefault();
 
-    var tbodyEl = document.querySelector("tbody")
-    var rowCount = tbodyEl.rows.length
-    var row = tbodyEl.deleteRow(0)
-})
+  var tbodyEl = document.querySelector("tbody");
+  var rowCount = tbodyEl.rows.length;
+  var row = tbodyEl.deleteRow(0);
+});
 
 istrintiPaskutini.addEventListener("click", function (e) {
-    e.preventDefault()
+  e.preventDefault();
 
-    var tbodyEl = document.querySelector("tbody")
-    var rowCount = tbodyEl.rows.length
-    var row = tbodyEl.deleteRow(rowCount - 1)
-})
-
+  var tbodyEl = document.querySelector("tbody");
+  var rowCount = tbodyEl.rows.length;
+  var row = tbodyEl.deleteRow(rowCount - 1);
+});
